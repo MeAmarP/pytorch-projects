c2e938aba535e7a72707a34d10ad40572a65dc50
Amar Potdar <Amar.Potdar@Honeywell.com>
2022-10-01 13:57:57 +0530

    minor change

--- classify/config.py
+++ classify/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 DATASET_DIR = Path.home().joinpath(Path("Documents/project/learning/datasets"))
-NAME_OF_DATASET = "concreate_cracks"
+NAME_OF_DATASET = "concreate_cracks/"
 PATH_TO_DATASET = Path.joinpath(DATASET_DIR).joinpath(NAME_OF_DATASET)
 
 if __name__ == "__main__":
